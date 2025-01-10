003598fb6fc19e16ce100936ca3620a1efd0228e
Ruairi O'Hare <rohare@ed.ac.uk>
2025-01-10 12:21:48 +0000

    added if for metadata not extracting

--- storage_service/locations/models/archipelago.py
+++ storage_service/locations/models/archipelago.py
@@ -61,7 +61,7 @@ class Archipelago(models.Model):
                 return metadata
         except Exception as e:
             LOGGER.error("Error reading metadata.json: %s", str(e))
-            return {}
+            return None
 
     def _upload_file(self, filename, source_path):
         """Uploads zip file to Archipelago before creating new entity
@@ -190,25 +190,28 @@ class Archipelago(models.Model):
             output_dir = os.path.dirname(input_path) + "/extracted/"
             os.makedirs(output_dir, exist_ok=True)
             metadata_json = self.read_metadata_json(metadata_json_path)
-            dc_fields = self.merge_dc_metadata(dc_fields, metadata_json)
-            strawberry = json.dumps(dc_fields)
+            if metadata_json is None:
+                strawberry = json.dumps(dc_fields)
+            else:
+                dc_fields = self.merge_dc_metadata(dc_fields, metadata_json)
+                strawberry = json.dumps(dc_fields)
 
-            LOGGER.info(f"Merged complete strawberry json is {strawberry}")
+                LOGGER.info(f"Merged complete strawberry json is {strawberry}")
 
-            # Write the merged metadata back into the XML (updating DC fields)
-            for dc_element in root.findall(".//dc:*", namespaces=namespaces):
-                field_name = dc_element.tag.split("}")[-1]
-                if field_name in dc_fields:
-                    new_value = dc_fields.get("field_" + field_name)
-                    if new_value:
-                        dc_element.text = new_value
+                # Write the merged metadata back into the XML (updating DC fields)
+                for dc_element in root.findall(".//dc:*", namespaces=namespaces):
+                    field_name = dc_element.tag.split("}")[-1]
+                    if field_name in dc_fields:
+                        new_value = dc_fields.get("field_" + field_name)
+                        if new_value:
+                            dc_element.text = new_value
 
-            # Save the updated XML back to the original input path
-            updated_xml = etree.tostring(
-                root, pretty_print=True, encoding="utf-8"
-            ).decode("utf-8")
-            with open(input_path, "w", encoding="utf-8") as file:
-                file.write(updated_xml)
+                # Save the updated XML back to the original input path
+                updated_xml = etree.tostring(
+                    root, pretty_print=True, encoding="utf-8"
+                ).decode("utf-8")
+                with open(input_path, "w", encoding="utf-8") as file:
+                    file.write(updated_xml)
 
             LOGGER.info(f"Merged complete strawberry json is {strawberry}")
             try:
